0a291257badf07d2dd771f7a747ca37c8e82d6d7
Louie Rivera IV <luigiriveraiv@gmail.com>
2022-08-25 16:11:50 -0700

    making production project

--- spotter/lib/main.dart
+++ spotter/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-int length = 0;
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,115 +11,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Spotter'),
-      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class Reminders extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _RemindersState();
-}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-class _RemindersState extends State<Reminders> {
-  List<bool> itemValues = List.generate(length, (index) => false); 
-  void _temp() {
+  void _incrementCounter() {
     setState(() {
-      itemValues.add(false);
-      length = length + 1;
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Reminders"),
-      ),
-      body: ListView.builder(
-        itemCount: length,
-        itemBuilder: (BuildContext context, int index) {
-          return CheckboxListTile(
-            title: Text('Item $index'), //label per entry
-            value: itemValues[index],
-            controlAffinity: ListTileControlAffinity.leading,
-            secondary: PopupMenuButton(
-              itemBuilder: ((context) => [
-                PopupMenuItem(
-                  child: Text("Delete"),
-                  value: 1)
-              ]),
-              icon: Icon(Icons.menu),
-            ),
-            onChanged: ((val) {
-              setState(() {
-                itemValues[index] = val!;
-              });
-            }),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _temp,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class Calendar extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _CalendarState();
-}
-
-class _CalendarState extends State<Calendar> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Calendar"),
-      ),
-    );
-  }
-}
-
-class Settings extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _SettingsState();
-}
-
-class _SettingsState extends State<Settings> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Settings"),
-      ),
-    );
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int page = 0;
-
-  final _pages = [
-    Reminders(),
-    Calendar(),
-    Settings(),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -132,27 +70,46 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      // appBar: AppBar(
-      //   // Here we take the value from the MyHomePage object that was created by
-      //   // the App.build method, and use it to set our appbar title.
-      //   title: Text(widget.title),
-      // ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: "Reminders"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_month), label: "Calendar"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: "Settings"),
-        ],
-        currentIndex: page,
-        onTap: (int index) {
-          setState(() {
-            page = index;
-          });
-        },
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: _pages[page],
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
