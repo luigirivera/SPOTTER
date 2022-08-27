185a97c86e276a3405ce976d0ebaee7e7bbecb18
Louie Rivera IV <luigirivera@users.noreply.github.com>
2022-08-27 15:32:12 -0700

    moved body widget to top

--- spotter/lib/main.dart
+++ spotter/lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
       endDrawer: const SettingsDrawer(),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
