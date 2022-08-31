5adf08b7d488bead6dbc5633668f484d2bd11b4d
Louie Rivera IV <luigirivera@users.noreply.github.com>
2022-08-30 23:20:40 -0700

    added about dialog box

--- spotter/lib/settings.dart
+++ spotter/lib/settings.dart
@@ -5,6 +5,41 @@ class SettingsDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Dialog errorDialog = Dialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0)), //this right here
+      child: Container(
+        height: 300.0,
+        width: 300.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Spotter - Tasks Companion"),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'COMP 499 Capstone Project',
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'By Arze Lu and Louie Rivera',
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 40.0)),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Done',
+                  style: TextStyle(color: Colors.black, fontSize: 18.0),
+                ))
+          ],
+        ),
+      ),
+    );
     return Drawer(
       child: Column(
         children: [
@@ -12,8 +47,12 @@ class SettingsDrawer extends StatelessWidget {
             child: Text('Settings'),
           ),
           ListTile(
-            title: const Text('Dark Mode'),
-            onTap: () => print("Dark mode tapped"),
+            title: const Text('Statistics'),
+            onTap: () => print("Statistics tapped"),
+          ),
+          ListTile(
+            title: const Text('Backup'),
+            onTap: () => print("Backup tapped"),
           ),
           // ListTile(
           //   title: const Text('Dark Mode'),
@@ -23,15 +62,13 @@ class SettingsDrawer extends StatelessWidget {
           //   ),
           // ),
           ListTile(
-            title: const Text('Notifications'),
-            trailing: Switch(
-              value: true,
-              onChanged: (value) {},
-            ),
-          ),
-          const ListTile(
             title: Text('About'),
             trailing: Icon(Icons.info),
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) => errorDialog);
+            },
           ),
         ],
       ),
