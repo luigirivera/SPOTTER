b31c572adcc954fd5fce14d734e438b5c84e545a
Hansly <arze2014@gmail.com>
2022-09-09 14:15:39 -0700

    changed files for firebase: manifest, gradle, etc

---
+++ spotter/lib/sample_task_data_buffer.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+///Making the Tasks object and filling it with some default values
+///These are subjected to change by the user so pls don't add final tag
+class Tasks{
+  Icon icon = const Icon(Icons.arrow_forward_ios, color: Colors.orange);
+
+}
