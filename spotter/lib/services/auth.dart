fce2c5489f74a516d32da96720467d3a1dff2ed6
Hansly <arze2014@gmail.com>
2022-09-09 15:47:50 -0700

    xgthjxfgh

---
+++ spotter/lib/services/auth.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  //sign in anon
+  Future anonSignIn() async{
+    try{
+      /** These class names are different than the demonstration */
+      UserCredential userCred = await _auth.signInAnonymously();
+      User? user = userCred.user;
+      return user;
+    }catch(e){
+
+    }
+  }
+  //sign in with email & password
+  //register with email & password
+  //sign out
+}
