1fd75275fb2a51b4f1ef51fb16c995f522458ec1
Hansly <arze2014@gmail.com>
2022-09-10 18:58:29 -0700

    more changes

--- spotter/lib/screens/wrapper.dart
+++ spotter/lib/screens/wrapper.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/user.dart';
 import 'authenticate/authenticate.dart';
 
 class Wrapper extends StatelessWidget {
@@ -6,6 +8,8 @@ class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<SpotterUser>(context);
+    print(user);
     //return either authenticate or home page
     return const Authenticate();
   }
