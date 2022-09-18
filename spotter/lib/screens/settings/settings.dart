5e9c7249871155813390ca036089bc6976b17499
Louie Rivera IV <luigirivera@users.noreply.github.com>
2022-09-18 16:48:41 -0700

    put back about

--- spotter/lib/screens/settings/settings.dart
+++ spotter/lib/screens/settings/settings.dart
@@ -16,6 +16,23 @@ class _SettingsDrawerState extends State<SettingsDrawer> {
   Widget build(BuildContext context) {
     Dialog aboutPopup = Dialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+      child: Container(
+        height: 150.0,
+        width: 150.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Spotter - Tasks Companion"),
+            Text('COMP 499 Capstone Project'),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'By Arze Lu and Louie Rivera',
+              ),
+            )
+          ],
+        ),
+      ),
     );
     return Drawer(
       child: Column(
