f315274d1581fbc169aefe1585d98f0575765630
Hansly <arze2014@gmail.com>
2022-09-23 09:48:26 -0700

    make the scrollable list buttons stays at top

--- spotter/lib/screens/home/tasks.dart
+++ spotter/lib/screens/home/tasks.dart
@@ -36,73 +36,58 @@ class TaskList extends StatefulWidget {
 }
 
 class _TaskListState extends State<TaskList> {
-
   @override
   Widget build(BuildContext context) {
     List<Task> taskList = objectbox.getTaskList();
-    return ListView.builder(
-              padding: const EdgeInsets.all(10),
-              itemCount: taskList.length * 2 + 1,
-              itemBuilder: (BuildContext context, int index) {
-                /** Creating a list of IconButtons,
-                 * then add the default plus button in to create a pop-up for adding new tasks.
-                 * Able to add more buttons if desired in the future
-                 */
-                if (index == 0) {
-                  List<IconButton> buttons = List.empty(growable: true);
-
-                  buttons.add(IconButton(
-                    icon: const Icon(
-                      Icons.add_circle,
-                      color: Colors.orange,
-                    ),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          barrierDismissible: true,
-                          builder: (context) {
-                            return AddTask(
-                              objectbox: objectbox,
-                            );
-                          }).then((value) {
-                        setState(() {});
-                      });
-                    },
-                    tooltip: 'Add new tasks',
-                  ));
-
-                  /** If page pop triggered,
-                   * then add the page pop button to the list
-                   */
-                  if (!widget.popped) {
-                    buttons.add(IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const TaskPopOutPage(),
-                            )).then((value) {
-                          setState(() {});
-                        });
-                      },
-                      icon: const Icon(Icons.output),
-                      tooltip: 'View in a pop out',
-                    ));
-                  }
-
-                  /** Adding the buttons in the list to the first row */
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: buttons.map((button) {
-                      return Container(
-                        child: (button),
+    bool popped = false;
+    return Column(children: [
+      SizedBox(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <IconButton>[
+            IconButton(
+              icon: const Icon(
+                Icons.add_circle,
+                color: Colors.orange,
+              ),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    builder: (context) {
+                      return AddTask(
+                        objectbox: objectbox,
                       );
-                    }).toList(),
-                  );
-                }
-
+                    }).then((value) {
+                  setState(() {});
+                });
+              },
+              tooltip: 'Add new tasks',
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const TaskPopOutPage(),
+                    )).then((value) {
+                  setState(() {});
+                });
+              },
+              icon: const Icon(Icons.output),
+              tooltip: 'View in a pop out',
+            ),
+          ],
+        ),
+      ),
+      Expanded(
+          child: ListView.builder(
+              padding: const EdgeInsets.all(10),
+              itemCount: taskList.length * 2,
+              itemBuilder: (BuildContext context, int index) {
                 if (index.isOdd) return const Divider();
-                final i = (index ~/ 2) - 1;
+                final i = index ~/ 2;
                 final check = taskList[i].completed;
 
                 /** Putting tasks onto the task board */
@@ -123,15 +108,20 @@ class _TaskListState extends State<TaskList> {
                     onPressed: () {
                       setState(() {
                         if (check) {
-                          //set data to false
+                          taskList[i].completed = false;
+
+                          ///Overwrite
+                          objectbox.addTask(taskList[i]);
                         } else {
-                          //set data to true
+                          taskList[i].completed = true;
+                          objectbox.addTask(taskList[i]);
                         }
                       });
                     },
                   ),
                 );
-              });
+              })),
+    ]);
   }
 }
 
