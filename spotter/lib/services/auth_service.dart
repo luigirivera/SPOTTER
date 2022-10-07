4cab42bc893ba761f62e25df744b631c35f8f879
Hansly <arze2014@gmail.com>
2022-10-07 00:27:42 -0700

    merge

--- spotter/lib/services/auth_service.dart
+++ spotter/lib/services/auth_service.dart
@@ -1,10 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:spotter/main.dart';
 import '../models/user_model.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final googleSignIn = GoogleSignIn();
+
+  Future googleLogin() async {
+    final googleUser = await googleSignIn.signIn();
+
+    if (googleUser == null) return;
+
+    final googleAuth = await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    await _auth.signInWithCredential(credential);
+  }
 
   //create user object based on User
   SpotterUser _spotterUser(User? user) {
